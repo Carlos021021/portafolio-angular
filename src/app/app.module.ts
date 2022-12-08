85135c9a02bfb33c251e0ff0515d3c87f79db72d
Carlos Luna Torres <juanc.lun@gmail.com>
2022-12-08 14:52:01 -0600

    Fin de los links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
+import { AppRoutingModule } from './shared/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { AboutComponent } from '../pages/about/about.component';
+import { ItemsComponent } from '../pages/items/items.component';
+import { PortafolioComponent } from '../pages/portafolio/portafolio.component';
+
+const app_routes: Routes = [
+    {path: '', component:PortafolioComponent },
+    {path: 'about', component:AboutComponent},
+    {path: 'items', component:ItemsComponent},
+    {path: '**' , pathMatch: 'full', redirectTo: ''}
+    
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot ( app_routes, { useHash: true} )
+    ],
+    exports: [
+        RouterModule  
+    ]
+
+})
+export class AppRoutingModule {
+
+}
