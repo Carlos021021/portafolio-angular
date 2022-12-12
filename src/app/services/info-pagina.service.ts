c18c4ae0593d9d00dfa11a04da846025123e40e3
Carlos Luna Torres <juanc.lun@gmail.com>
2022-12-11 23:10:05 -0600

    Carga de informacion de firebase al html

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,20 +10,32 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any = [];
   constructor( private http: HttpClient) { 
     // console.log('Servicio de InfoPagina listo');
 
+      this.cargarInfo();
+      this.cargarEquipo();
+   
+  }
 
+  private cargarInfo () {
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
 
-      console.log(resp);
+      // console.log(resp);
       // console.log( resp['email'] );
+    });
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://angular-1-7f526-default-rtdb.firebaseio.com/equipo.json').subscribe((resp: any) => {
 
-
+      this.equipo = resp;
+      // console.log(resp);
+      // console.log( resp['email'] );
     });
   }
 }
